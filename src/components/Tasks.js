f446395b4d40cda25997877ecf8d35df1936e086
Raghav <codingkapur@gmail.com>
2021-10-20 18:50:14 +0530

    display message when no tasks found.

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,22 @@
 import Task from "./Task";
 
-const Tasks = ({ tasks, deleteTask, toggleEditMode, toggleStatus}) => {
+const Tasks = ({ tasks, deleteTask, toggleEditMode, toggleStatus }) => {
   return (
     <div className="tasks__container">
-      {tasks.map((task, index) => {
-        return <Task key={task._id} task={task} index={index} deleteTask={deleteTask} toggleEditMode={toggleEditMode} toggleStatus={toggleStatus}/>;
-      })}
+      {tasks.length
+        ? tasks.map((task, index) => {
+            return (
+              <Task
+                key={task._id}
+                task={task}
+                index={index}
+                deleteTask={deleteTask}
+                toggleEditMode={toggleEditMode}
+                toggleStatus={toggleStatus}
+              />
+            );
+          })
+        : "NO TASKS FOUND"}
     </div>
   );
 };
